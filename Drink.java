91887ecd631ca5eb7c052172771508014fd3b4b7
nampole <nampole@aol.com>
2018-10-11 18:05:08 +0900

    1st

---
+++ Drink.java
@@ -0,0 +1,33 @@
+package com.hfad.starbuzz;
+
+public class Drink {
+    private String name;
+    private String description;
+    private int imageResourceId;
+
+    //세가지 음료 배열
+    public static final Drink[] drinks = {
+            new Drink ("라떼","따뜻한우유와에스프레소샷2개",R.drawable.latte),
+            new Drink ("카푸치노","따뜻한우유와에스프레소샷2개",R.drawable.cappuccino),
+            new Drink ("필터","따뜻한우유와에스프레소샷2개",R.drawable.filter),
+    };
+
+    private Drink(String name, String description, int imageResourceId) {
+        this.name = name;
+        this.description = description;
+        this.imageResourceId = imageResourceId;
+    }
+
+    public String getDescription(){
+        return description;
+    }
+    public String getName(){
+        return name;
+    }
+    public String getImageResourceId(){
+        return imageResourceId;
+    }
+    public String toString(){
+        return this.name;
+    }
+}
